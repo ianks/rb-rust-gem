914fde3ec912740e3912c93a2dd5ae1aba2f5205
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2022-03-14 16:26:25 -0400

    Try x86_64

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 IFS=$'\n\t'
 
 setup_ruby() {
-  export RUBY="$HOME/.rake-compiler/ruby/arm-linux-gnueabihf/$RUBY_VERSION/bin/ruby"
-  export GEM_BIN="$HOME/.rake-compiler/ruby/arm-linux-gnueabihf/$RUBY_VERSION/bin/gem"
+  export RUBY="$HOME/.rake-compiler/ruby/x86_64-redhat-linux/$RUBY_VERSION/bin/ruby"
+  export GEM_BIN="$HOME/.rake-compiler/ruby/x86_64-redhat-linux/$RUBY_VERSION/bin/gem"
   echo "Using $RUBY_VERSION"
 }
 
@@ -13,7 +13,7 @@ upgrade_rubygems() {
   git clone --single-branch --branch cargo-builder-target --depth 1 https://github.com/ianks/rubygems /tmp/rubygems
   pushd /tmp/rubygems
   # "$HOME/.rake-compiler/ruby/arm-linux-gnueabihf/$RUBY_VERSION/bin/ruby" setup.rb
-  "$HOME/.rake-compiler/ruby/x86_64-redhat-linux/$RUBY_VERSION/bin/ruby" setup.rb
+  "$RUBY" setup.rb
   popd
   rm -rf /tmp/rubygems
 }
