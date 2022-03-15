9494d7104ed693cd1bdbbafe5b0ef1b3fc22715f
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-03-15 09:33:17 -0400

    extconf

--- examples/rust_ruby_example/ext/rust_ruby_example/extconf.rb
+++ examples/rust_ruby_example/ext/rust_ruby_example/extconf.rb
@@ -2,13 +2,14 @@ require 'mkmf'
 require 'rubygems/ext'
 require 'rubygems/ext/cargo_builder'
 
-dest_path = __dir__
+target = "rust_ruby_example"
+dest_path = File.join( Dir.pwd, "target")
 results = []
 args = []
-lib_dir = __dir__
+lib_dir = Dir.pwd 
 cargo_dir = File.expand_path('../..', __dir__)
-puts cargo_dir
-spec = Struct.new(:name, :metadata).new('rust_ruby_example', {})
+spec = Struct.new(:name, :metadata).new(target, {})
+
 begin
   Gem::Ext::CargoBuilder.new(spec).build(nil, dest_path, results, args, lib_dir, cargo_dir)
 rescue StandardError => e
@@ -16,4 +17,18 @@ rescue StandardError => e
   raise
 end
 
-File.write('Makefile', dummy_makefile(__dir__))
+make_install = <<~EOF
+  target_prefix = /#{target}
+
+  TARGET = #{target}
+  DLLIB = $(TARGET).bundle
+  RUBYARCHDIR   = $(sitearchdir)$(target_prefix)
+  CLEANLIBS = release/
+
+  #{dummy_makefile(__dir__).join("\n").gsub("all install static install-so install-rb", "all static install-so install-rb")}
+
+  install: $(DLLIB)
+  \t$(INSTALL_PROG) $(DLLIB) $(RUBYARCHDIR)
+EOF
+
+File.write('Makefile', make_install)
