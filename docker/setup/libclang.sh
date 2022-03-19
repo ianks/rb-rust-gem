4ad0e3541d072434dd5c0c823a7161dc500f2a61
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-03-18 23:26:59 -0400

    f

--- docker/setup/libclang.sh
+++ docker/setup/libclang.sh
@@ -32,7 +32,7 @@ main() {
   builtin cd lld
   mkdir -p build-release
   builtin cd build-release
-  cmake .. -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$install_dir -DCMAKE_PREFIX_PATH=$install_dir -DCMAKE_BUILD_TYPE=Release -DLLVM_PARALLEL_LINK_JOBS=1 -DCMAKE_CXX_STANDARD=17 -DLLVM_TARGETS_TO_BUILD=i386
+  cmake .. -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$install_dir -DCMAKE_PREFIX_PATH=$install_dir -DCMAKE_BUILD_TYPE=Release -DLLVM_PARALLEL_LINK_JOBS=1 -DCMAKE_CXX_STANDARD=17 -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=i386
   make -j "$(nproc)" install
   builtin cd ../..
 
