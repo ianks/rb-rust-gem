1f29e81fa27c39bcc0fb561cead3d7bb1ca8d6d5
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-03-18 23:23:47 -0400

    f

--- docker/setup/libclang.sh
+++ docker/setup/libclang.sh
@@ -24,7 +24,7 @@ main() {
   builtin cd llvm
   mkdir -p build-release
   builtin cd build-release
-  cmake .. -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$install_dir -DCMAKE_PREFIX_PATH=$install_dir -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_PARALLEL_LINK_JOBS=1
+  cmake .. -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$install_dir -DCMAKE_PREFIX_PATH=$install_dir -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_TARGETS_TO_BUILD=i386
   make -j "$(nproc)" install
   builtin cd ../..
 
@@ -32,7 +32,7 @@ main() {
   builtin cd lld
   mkdir -p build-release
   builtin cd build-release
-  cmake .. -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$install_dir -DCMAKE_PREFIX_PATH=$install_dir -DCMAKE_BUILD_TYPE=Release -DLLVM_PARALLEL_LINK_JOBS=1 -DCMAKE_CXX_STANDARD=17
+  cmake .. -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$install_dir -DCMAKE_PREFIX_PATH=$install_dir -DCMAKE_BUILD_TYPE=Release -DLLVM_PARALLEL_LINK_JOBS=1 -DCMAKE_CXX_STANDARD=17 -DLLVM_TARGETS_TO_BUILD=i386
   make -j "$(nproc)" install
   builtin cd ../..
 
@@ -40,7 +40,7 @@ main() {
   builtin cd clang
   mkdir -p build-release
   builtin cd build-release
-  cmake .. -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$install_dir -DCMAKE_PREFIX_PATH=$install_dir -DCMAKE_BUILD_TYPE=Release -DLLVM_PARALLEL_LINK_JOBS=1
+  cmake .. -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$install_dir -DCMAKE_PREFIX_PATH=$install_dir -DCMAKE_BUILD_TYPE=Release -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_TARGETS_TO_BUILD=i386
   make -j "$(nproc)" install
   builtin cd ../..
 
