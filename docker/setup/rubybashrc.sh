7fd769808b03931c06b060c73b17931c1b071f1e
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-03-28 23:19:54 -0400

    f

--- docker/setup/rubybashrc.sh
+++ docker/setup/rubybashrc.sh
@@ -12,7 +12,7 @@ main() {
   echo "export CARGO_HOME=\"$CARGO_HOME\"" >> /etc/rubybashrc
   echo "export RUBY_TARGET=\"$RUBY_TARGET\"" >> /etc/rubybashrc
   echo "export RUST_TARGET=\"$RUST_TARGET\"" >> /etc/rubybashrc
-  echo "export RUST_TOOLCHAIN=\"$RUST_TOOLCHAIN\"" >> /etc/rubybashrc
+  echo "export RUSTUP_DEFAULT_TOOLCHAIN=\"$RUSTUP_DEFAULT_TOOLCHAIN\"" >> /etc/rubybashrc
   echo "export BINDGEN_EXTRA_CLANG_ARGS=\"$BINDGEN_EXTRA_CLANG_ARGS\"" >> /etc/rubybashrc
   echo "export PKG_CONFIG_ALLOW_CROSS=\"$PKG_CONFIG_ALLOW_CROSS\"" >> /etc/rubybashrc
   echo "export LIBCLANG_PATH=\"$LIBCLANG_PATH\"" >> /etc/rubybashrc
